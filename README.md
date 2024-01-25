314480b61982468c6bce0155916b640f0b40d44c
oskarscot <oskar.budzisz4@gmail.com>
2024-01-25 12:37:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Software Engineering Methods 2024
+Repository for the Software Engineering Methods module - Edinburgh Napier University
---
+++ src/main/java/scot/oskar/sem/Application.java
@@ -0,0 +1,8 @@
+package scot.oskar.sem;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Software Engineering Methods 2024");
+    }
+}
